454a8c4c3fa67cead4b0b8ce117835aa10385640
aurehmaan <71068738+aurehmaan@users.noreply.github.com>
2022-04-14 19:44:14 +0500

    Add files via upload

---
+++ Script1.ps1
@@ -0,0 +1,9 @@
+# Add Web Server Feature
+
+Add-WindowsFeature Web-Server
+
+ 
+
+# Create file Default.htm and add some sample text
+
+Set-Content -Path C:\inetpub\wwwroot\Default.htm -Value "Hello! This Machine is in Azure East US Zone. Computer Name is $($env:computername) " 
